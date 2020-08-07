0ff626656d0c17728347475429d3e174afc7b7b8
rm-mplpl-ebi <66986303+rm-mplpl-ebi@users.noreply.github.com>
2020-08-07 13:23:43 +0900

    Update script.js

--- p2p-media/script.js
+++ p2p-media/script.js
@@ -6,40 +6,38 @@ const Peer = window.Peer;
     'use strict';
 
     //htmlのidからデータを取得
-    //取得したデータを変数に代入
     var timer = document.getElementById('timer');
     var start = document.getElementById('start');
     var stop = document.getElementById('stop');
     var reset = document.getElementById('reset');
 
-    //クリック時の時間を保持するための変数定義
+    //クリック時の時間を保持するための変数
     var startTime;
-    //経過時刻を更新するための変数。 初めはだから0で初期化
+    //経過時刻を更新するための変数,0で初期化
     var elapsedTime = 0;
     //タイマーを止めるにはclearTimeoutを使う必要があり、そのためにはclearTimeoutの引数に渡すためのタイマーのidが必要
     var timerId;
-    //タイマーをストップ -> 再開させたら0になってしまうのを避けるための変数。
+    //タイマーをストップ -> 再開させたら0になってしまうのを避けるための変数
     var timeToadd = 0;
-    //ミリ秒の表示ではなく、分とか秒に直すための関数, 他のところからも呼び出すので別関数として作る
-    //計算方法として135200ミリ秒経過したとしてそれを分とか秒に直すと -> 02:15:200
+    //ミリ秒→分，秒に変換
+    //例：135200ミリ秒経過 -> 02:15:200
     function updateTimetText(){
-        //m(分) = 135200 / 60000ミリ秒で割った数の商　-> 2分
+        //m(分)
         var m = Math.floor(elapsedTime / 60000);
-        //s(秒) = 135200 % 60000ミリ秒で / 1000 (ミリ秒なので1000で割ってやる) -> 15秒
+        //s(秒)
         var s = Math.floor(elapsedTime % 60000 / 1000);
-        //ms(ミリ秒) = 135200ミリ秒を % 1000ミリ秒で割った数の余り
+        //ms(ミリ秒）
         var ms = elapsedTime % 1000;
         //HTML 上で表示の際の桁数を固定する　例）3 => 03　、 12 -> 012
-        //javascriptでは文字列数列を連結すると文字列になる
-        //文字列の末尾2桁を表示したいのでsliceで負の値(-2)引数で渡してやる。
+        //文字列の末尾2桁を表示したいのでsliceで負の値(-2)引数で渡すといいかんじ
         m = ('0' + m).slice(-2); 
         s = ('0' + s).slice(-2);
         ms = ('0' + ms).slice(-3);
-        //HTMLのid　timer部分に表示させる　
+        //HTMLのid　timer部分に表示
         timer.textContent = m + ':' + s + ':' + ms;
     }
 
-    //再帰的に使える用の関数
+    //カウントアップする
     function countUp(){
         //timerId変数はsetTimeoutの返り値になるので代入する
         timerId = setTimeout(function(){
@@ -52,35 +50,36 @@ const Peer = window.Peer;
         },10);
     }
 
-    //startボタンにクリック時のイベントを追加(タイマースタートイベント)
+    //タイマースタート
     start.addEventListener('click',function(){
-        //在時刻を示すDate.nowを代入
+        //現在時刻Date.nowを代入
         startTime = Date.now();
-        //再帰的に使えるように関数を作る
+        //再帰的に上の関数を呼び出し
         countUp();
     });
 
-    //stopボタンにクリック時のイベントを追加(タイマーストップイベント)
+    //タイマーストップ
     stop.addEventListener('click',function(){
         //タイマーを止めるにはclearTimeoutを使う必要があり、そのためにはclearTimeoutの引数に渡すためのタイマーのidが必要
        clearTimeout(timerId);
-        //タイマーに表示される時間elapsedTimeが現在時刻かたスタートボタンを押した時刻を引いたものなので、
+        //タイマーに表示される時間elapsedTimeが現在時刻からスタートボタンを押した時刻を引いたもの
         //タイマーを再開させたら0になってしまう。elapsedTime = Date.now - startTime
-        //それを回避するためには過去のスタート時間からストップ時間までの経過時間を足してあげなければならない。elapsedTime = Date.now - startTime + timeToadd (timeToadd = ストップを押した時刻(Date.now)から直近のスタート時刻(startTime)を引く)
+        //過去のスタート時間からストップ時間までの経過時間を足す　例：elapsedTime = Date.now - startTime + timeToadd (timeToadd = ストップを押した時刻(Date.now)から直近のスタート時刻(startTime)を引く)
        timeToadd += Date.now() - startTime;
     });
 
-    //resetボタンにクリック時のイベントを追加(タイマーリセットイベント)
+    //タイマーリセット
     reset.addEventListener('click',function(){
-        //経過時刻を更新するための変数elapsedTimeを0にしてあげつつ、updateTimetTextで0になったタイムを表示。
+        //経過時刻を更新するための変数elapsedTimeを0にしてあげつつ、updateTimetTextで0になったタイムを表示
         elapsedTime = 0;
-        //リセット時に0に初期化したいのでリセットを押した際に0を代入してあげる
+        //リセット時に0に初期化したいのでリセットを押した際に0を代入
         timeToadd = 0;
         //updateTimetTextで0になったタイムを表示
         updateTimetText();
     });
 })();
 
+//ビデオ通話の処理
 (async function main() {
   const localVideo = document.getElementById('js-local-stream');
   const localId = document.getElementById('js-local-id');
